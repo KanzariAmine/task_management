3da886e6f759ba31dcffd68cae7036d0d1ef81cc
amine_kanzari <kanzari.amin@gmail.com>
2024-05-18 11:31:25 +0100

    create Dashboard screen

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,67 @@
-// import React from 'react';
+/* eslint-disable import/no-unresolved */
 
+import Card from '@components/Card';
+import Chart from '@components/Chart';
+import TaskTable from '@components/TaskTable';
+import UserTable from '@components/UserTable';
+import { FaNewspaper } from 'react-icons/fa';
+import { FaArrowsToDot } from 'react-icons/fa6';
+import { LuClipboardEdit } from 'react-icons/lu';
+import { MdAdminPanelSettings } from 'react-icons/md';
+import { summary } from '../assets/data';
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const totals = summary.tasks;
+  const stats = [
+    {
+      _id: '1',
+      label: 'TOTAL TASK',
+      total: summary?.totalTasks || 0,
+      icon: <FaNewspaper />,
+      bg: 'bg-[#1d4ed8]',
+    },
+    {
+      _id: '2',
+      label: 'COMPLTED TASK',
+      total: totals['completed'] || 0,
+      icon: <MdAdminPanelSettings />,
+      bg: 'bg-[#0f766e]',
+    },
+    {
+      _id: '3',
+      label: 'TASK IN PROGRESS ',
+      total: totals['in progress'] || 0,
+      icon: <LuClipboardEdit />,
+      bg: 'bg-[#f59e0b]',
+    },
+    {
+      _id: '4',
+      label: 'TODOS',
+      total: totals['todo'],
+      icon: <FaArrowsToDot />,
+      bg: 'bg-[#be185d]' || 0,
+    },
+  ];
+
+  return (
+    <div className="h-full py-4">
+      {' '}
+      <div className=" grid grid-cols-1 md:grid-cols-4 gap-5">
+        {stats.map(({ icon, bg, label, total }, index) => (
+          <Card key={index} icon={icon} bg={bg} label={label} count={total} />
+        ))}
+      </div>
+      <div className="w-full bg-white my-16 p-4 rounded shadow-sm">
+        <h4 className="text-xl text-gray-600 font-semibold">Chart by Priority</h4>
+        <Chart />
+      </div>
+      <div className="w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8">
+        {/* Left */}
+        <TaskTable tasks={summary.last10Task} />
+        {/* Right */}
+        <UserTable users={summary.users} />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
